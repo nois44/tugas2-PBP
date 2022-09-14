2e7bbc82a68890eeff6e31fb46327e56d50ab683
GEdgar <gabrieledgarfn@gmail.com>
2022-09-14 17:29:55 +0700

    Tugas PBP 2

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('katalog/', include('katalog.urls')),
+    path('', include('katalog.urls')),
 ]
 
 # TODO: Implement Routings Here
