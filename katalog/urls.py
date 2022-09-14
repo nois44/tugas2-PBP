dc58f04153716e85c2e4ec699078f3311390e51e
GEdgar <gabrieledgarfn@gmail.com>
2022-09-14 17:14:16 +0700

    Tugas PBP 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import katalog
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', katalog, name='katalog')
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def katalog(request): 
+    return render(request, 'katalog.html', context)
+
+data_barang_katalog = CatalogItem.objects.all()
+context = {
+    'list_katalog' : data_barang_katalog, 
+    'nama' : 'Gabriel Edgar Firdausyah Nugroho', 
+    'npm' : '2106752312'
+}
