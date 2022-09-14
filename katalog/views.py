6dcce76629c44ed175cce66d3ef4ff1bcd8317ad
GEdgar <gabrieledgarfn@gmail.com>
2022-09-14 22:12:19 +0700

    Tugas PBP 2

--- katalog/views.py
+++ katalog/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 from katalog.models import CatalogItem
 
 # TODO: Create your views here.
-def katalog(request): 
+def katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog' : data_barang_katalog, 
+        'nama' : 'Gabriel Edgar Firdausyah Nugroho', 
+        'npm' : '2106752312'
+    }
     return render(request, 'katalog.html', context)
 
-data_barang_katalog = CatalogItem.objects.all()
-context = {
-    'list_katalog' : data_barang_katalog, 
-    'nama' : 'Gabriel Edgar Firdausyah Nugroho', 
-    'npm' : '2106752312'
-}
+
